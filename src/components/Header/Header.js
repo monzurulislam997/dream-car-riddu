1523b30835543dfb9f82ae76079cdb946f7c146a
fname lname <monzurulislam997@gmail.com>
2022-05-30 00:11:30 +0600

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,11 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <nav>
-                <Link to='/home'>HOME</Link>
-                <Link to='/reviews'>REVIEWS</Link>
-                <Link to='/dashboard'>DASHBOARD</Link>
-                <Link to='/blogs'>BLOGS</Link>
+            <nav className='m-4 '>
+                <Link className='hover:text-orange-600' to='/home'>HOME</Link>
+                <Link className='hover:text-orange-600' to='/reviews'>REVIEWS</Link>
+                <Link className='hover:text-orange-600' to='/dashboard'>DASHBOARD</Link>
+                <Link className='hover:text-orange-600' to='/blogs'>BLOGS</Link>
             </nav>
 
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,10 @@ import './Home.css'
 const Home = () => {
     return (
         <div className='home-container'>
-            <div className='home-heading'>
-                <h1>Bugatti Will Be Yours</h1>
-                <h1>Just For $3.2 Milion!! </h1>
+            <div className='home-heading mt-24'>
+                <h1 className='text-6xl font-bold text-violet-800 '>BUGATTI Will Be Yours</h1>
+                <h1 className='text-6xl font-bold '>Just For $3.2 Milion!! </h1>
+                <p className='text-gray-800 font-semibold mt-4 p-6'>Life is one.Money is temporary.Don't be miser.Just Full-Fill your dream.Developed with learnings from both the 300+ and the Pur Sport, the Super Sport is the Chiron lineup's tour de force.</p>
             </div>
             <div className='img-container'>
                 <img src={car} alt="" />
